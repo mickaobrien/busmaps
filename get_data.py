2e4e5ba91a3feecc86cd7c1efd00ce3848667fbe
Mick O'Brien <mickaobrien@gmail.com>
2016-05-01 16:23:13 +0100

    demo get_route_details

--- get_data.py
+++ get_data.py
@@ -2,6 +2,13 @@ import json
 import requests
 
 
+def get_route_details():
+    url = 'http://rtpi.ie/ConnectService.svc/GetServiceDataSerialized'
+    data = '{"publicServiceCode":"202","operatorID":2,"depotID":1,"serviceVariantIDs":"866,867,873,874,875","routeLineNodes":"none"}'
+    route = get_data(url, data)
+    return route['AllStops']
+
+
 def get_route_options(route_number):
     url = 'http://rtpi.ie/ConnectService.svc/GetPublicServicesForCriteriaSerialized'
     data = '{"searchString": %d, "districtId": -1}' % route_number
