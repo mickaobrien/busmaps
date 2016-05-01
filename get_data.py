9cef7420116b40c431f679eb54c83216c61ac933
Mick O'Brien <mickaobrien@gmail.com>
2016-05-01 16:20:25 +0100

    extract get_data

--- get_data.py
+++ get_data.py
@@ -1,21 +1,22 @@
 import json
 import requests
 
-def get_route_details(route_number):
+
+def get_route_options(route_number):
     url = 'http://rtpi.ie/ConnectService.svc/GetPublicServicesForCriteriaSerialized'
     data = '{"searchString": %d, "districtId": -1}' % route_number
-    r = requests.post(url, data=data, headers={'content-type': 'application/json; charset=UTF-8'})
-    if not r.ok:
-        print 'Couldn\'t get data for route {}'.format(route_number)
-    json_string = r.content.replace('\\', '').split('=')[1][:-2]
-    return json.loads(json_string)
+    return get_data(url, data)
 
 
 def get_stop_details(stop_number):
     url = 'http://rtpi.ie/ConnectService.svc/GetStopsFromStopRef'
     data = '{"stopref": %d}' % stop_number
+    return get_data(url, data)
+
+
+def get_data(url, data):
     r = requests.post(url, data=data, headers={'content-type': 'application/json; charset=UTF-8'})
     if not r.ok:
-        print 'Couldn\'t get data for stop {}'.format(route_number)
+        print 'Couldn\'t get data:\n url: {}\n data: {}'.format(url, data)
     json_string = r.content.replace('\\', '').split('=')[1][:-2]
     return json.loads(json_string)
